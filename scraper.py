e50bc3c1a472b82a400c0e069c1127a4e9020514
Nathan Pham <nathanp3@uci.edu>
2025-10-27 19:31:35 -0700

    Basic info

--- scraper.py
+++ scraper.py
@@ -5,6 +5,40 @@ def scraper(url, resp):
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
 
+# A set of common English stop words to ignore
+stopwords = {
+    "a", "about", "above", "after", "again", "against", "all", "am", "an", "and",
+    "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being",
+    "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't",
+    "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during",
+    "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't",
+    "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here",
+    "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i",
+    "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's",
+    "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no",
+    "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our",
+    "ours", "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd",
+    "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that",
+    "that's", "the", "their", "theirs", "them", "themselves", "then", "there",
+    "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this",
+    "those", "through", "to", "too", "under", "until", "up", "very", "was",
+    "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what",
+    "what's", "when", "when's", "where", "where's", "which", "while", "who",
+    "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you",
+    "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"
+}
+
+# Global word frequency map
+word_frequency_map = dict()
+
+# Keep a set of all the pages here so we can analyze subdomains later
+pages_set = set()
+
+"""
+- Make sure to defragment the URLs
+- Use BeautifulSoup to extract links and content
+- Save URLs and web page content to disk for parsing in report
+"""
 def extract_next_links(url, resp):
     # Implementation required.
     # url: the URL that was used to get the page
@@ -17,6 +51,10 @@ def extract_next_links(url, resp):
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
     return list()
 
+
+"""
+- Only URLs that are within the domains and paths
+"""
 def is_valid(url):
     # Decide whether to crawl this url or not. 
     # If you decide to crawl it, return True; otherwise return False.
@@ -38,3 +76,22 @@ def is_valid(url):
     except TypeError:
         print ("TypeError for ", parsed)
         raise
+
+
+"""
+Things to remember
+- Only crawl the following domains:
+    - *.ics.uci.edu/*
+    - *.cs.uci.edu/*
+    - *.informatics.uci.edu/*
+    - *.stat.uci.edu/*
+- Keep track of unique pages
+    - Uniqueness: only established by the URL and discarding the fragment
+- Keep track of longest page in terms of the number of words (HTML markup doesn't count)
+- What are the 50 most common words in the entire set of pages crawled under these domains? (Ignore english stop words)
+    - Keep a global frequency count of all words seen so far besides stop words
+- Keep track of how many subdomains found in the uci.edu domain
+    - List subdomains and the number of unique pages in each subdomain
+    - This can be done by keeping a list of all the unique pages
+        - Parse the list and get subdomains and keep a frequency of the pages while parsing
+"""
